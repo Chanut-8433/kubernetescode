b19cd977e97a759e5f0db16640ba9796a110a7b3
TOp <85511779+Chanut-8433@users.noreply.github.com>
2025-09-25 16:34:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Route หลัก
 @app.route('/')
 def home():
-    return 'Linuxgray Training Center'
+    return 'Linuxgray Training Center-1'
 
 # Route สำหรับ health check (ใช้กับ Docker/K8s ได้ดี)
 @app.route('/health')
