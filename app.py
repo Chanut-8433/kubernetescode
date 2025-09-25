0ce75dd43ddbb2cec76775124d1601ea47cfbb33
TOp <85511779+Chanut-8433@users.noreply.github.com>
2025-09-25 15:48:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-from flask import Flask, jsonify
-
-# สร้าง Flask instance
+from flask import Flask
 app = Flask(__name__)
 
-# Route หลัก
 @app.route('/')
-def home():
-    return 'Linuxgray Training Center - 3'
-
-# Route สำหรับ health check (ใช้กับ Docker/K8s ได้ดี)
-@app.route('/health')
-def health():
-    return jsonify(status="UP"), 200
-
-# รันเซิร์ฟเวอร์
-if __name__ == '__main__':
-    # host=0.0.0.0 เปิดให้ container/network ภายนอกเข้าถึงได้
-    app.run(host='0.0.0.0', port=5000, debug=True)
+def hello_world():
+    return 'Linuxgray Training Center-2'
